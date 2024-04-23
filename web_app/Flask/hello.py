9b31a5f281ed793ffcdf64ce52422ac06919f3fc
Paweł Gliwny <p.gliwny@gmail.com>
2024-04-23 22:27:14 +0200

    hello flask example

---
+++ web_app/Flask/hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello_page():
+    return "<p>Hello, World!!!!</p>"
+
+@app.route("/")
+def hello_flask():
+    return "<p>Hello, Flask!</p>"
+
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+   return 'Blog Number %d' % postID
+
+if __name__ == '__main__':
+   app.run(debug=True)
