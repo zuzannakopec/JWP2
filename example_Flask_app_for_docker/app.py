c113154f18d106b59cc567688d891350cab4a875
Zuzanna Kopeć <ul0246826@edu.uni.lodz.pl>
2024-05-25 10:12:29 +0200

    wip

--- example_Flask_app_for_docker/app.py
+++ example_Flask_app_for_docker/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
-from models import db, Task
+from models import db, Task, Teacher
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
+
 
 @app.route('/add', methods=['POST'])
 def add_task():
@@ -19,6 +21,7 @@ def add_task():
         db.session.commit()
     return redirect(url_for('index'))
 
+
 @app.route('/delete/<int:task_id>')
 def delete_task(task_id):
     task = Task.query.get(task_id)
@@ -26,6 +29,28 @@ def delete_task(task_id):
         db.session.delete(task)
         db.session.commit()
     return redirect(url_for('index'))
+
+
+@app.route('/addTeacher', methods=['POST'])
+def add_teacher():
+    name = request.form.get('name')
+    subject = request.form.get('subject')
+    time = request.form.get('time')
+    if name:
+        new_teacher = Teacher(name=name, subject=subject, time=time)
+        db.session.add(new_teacher)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<int:teacher_id>')
+def delete_teacher(teacher_id):
+    teacher = Teacher.query.get(teacher_id)
+    if teacher:
+        db.session.delete(teacher)
+        db.session.commit()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     with app.app_context():
--- example_Flask_app_for_docker/models.py
+++ example_Flask_app_for_docker/models.py
@@ -2,6 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
+
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    subject = db.Column(db.String(80), nullable=False)
+    time = db.Column(db.String(80), nullable=False)
+
+
